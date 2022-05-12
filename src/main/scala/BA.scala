f8bec46fc5ea1fa348f74c27d6265c2aff79a2bb
Adam-Vandervorst <adam.vandervorst@gmail.com>
2022-05-12 16:02:32 +0200

    Upgrade BA to use DNIELMG

--- src/main/scala/BA.scala
+++ src/main/scala/BA.scala
@@ -1,37 +1,30 @@
-import be.adamv.picograph.graphs.DNIELG.{DNIELG, given}
 import be.adamv.picograph.graphs.DNIELMG.{DNIELMG, given}
 import be.adamv.picograph.graphs.PatchG.{PatchG, DSL, RewriteRule}
 import be.adamv.picograph.conversions.split
-import be.adamv.picograph.{Rooted, nodeId}
+import be.adamv.picograph.nodeId
 
 
 class BA[Prop]:
-  private val g = DNIELG[Set[Prop]]()
-  private given r: Rooted[g.type, State] with
-    extension (ev: g.type)
-      def root: State = init
+  val g = DNIELMG[Prop]()
 
   opaque type State = g.Node
   val init: State = g.newNode()
 
   inline def newState(): State = g.newNode()
   inline def states: Iterator[State] = g.nodeIt
-  inline def proper_states: Iterator[State] = r.nonRootNodeIt(g.asInstanceOf)
-  inline def connect(src: State, dst: State, props: Set[Prop]): Unit = g.connect(src, dst, props)
+  inline def proper_states: Iterator[State] = states.filter(_ != init)
+  inline def connect(src: State, dst: State, props: Set[Prop]): Unit = props.foreach(prop => g.connect(src, dst, prop))
 
   def plot(): Unit =
     println(s"${init.nodeId} [peripheries=2];")
-    for n <- r.nonRootNodeIt(g) do
+    for n <- proper_states do
       println(s"${n.nodeId};")
-    for (s, ls, t) <- g.edgeIt do
-      println(s"${s.nodeId} -> ${t.nodeId} [label=\"${ls.mkString(" ")}\"];")
+    for (s, t) <- g.edgeIt do
+      println(s"${s.nodeId} -> ${t.nodeId} [label=\"${g.labeling(s, t).mkString(" ")}\"];")
 
-  def rewriteIntoSelf(): Unit =
-    // NOTE: Demo
+  def rewriteIntoSelf(): Iterator[DNIELMG[Prop]] =
     import language.postfixOps
     import DSL.*
-    val mg: DNIELMG[Set[Prop]] = g.split(Set(_))
-
     val lhs = DNIELMG[Char]()
     val l = lhs.newNodes(2)
     lhs.connect(l(0), l(1), 'a')
@@ -50,11 +43,5 @@ class BA[Prop]:
       (pg, Seq(<>-(r(0)), <>-(r(0))))
 
     val indexMap = Map(0 -> 0, 1 -> 1)
-    val rr = RewriteRule(lpg, rpg)(indexMap.map((r, l) => rpts(r) -> lpts(l)))
-
-    val todo1 = mg.occurrencesNaive(lhs).toList(1)
-    val mg1 = rr.apply(mg, todo1)
-
-    val todo2 = mg1.occurrencesNaive(lhs).toList(2)
-    val mg2 = rr.apply(mg1, todo2)
-    mg2.plot()
+    RewriteRule(lpg, rpg)(indexMap.map((r, l) => rpts(r) -> lpts(l)))
+      .applyIt(g)

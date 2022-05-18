760773ab949deb6d928eb367681cde48e7708983
Adam-Vandervorst <adam.vandervorst@gmail.com>
2022-05-18 18:01:13 +0200

    Fix multigraph representation

--- src/main/scala/BA.scala
+++ src/main/scala/BA.scala
@@ -1,29 +1,29 @@
 import be.adamv.picograph.graphs.DNIELMG.{DNIELMG, given}
-import be.adamv.picograph.graphs.PatchG.{PatchG, C, RewriteRule}
+import be.adamv.picograph.graphs.PatchG.{C, PatchG, RewriteRule}
 import be.adamv.picograph.conversions.split
 import be.adamv.picograph.nodeId
 
 
 class BA[Prop]:
-  val g = DNIELMG[Prop]()
+  val g = DNIELMG[Set[Prop]]()
 
   opaque type State = g.Node
   val init: State = g.newNode()
 
   inline def newState(): State = g.newNode()
-  inline def states: Iterator[State] = g.nodeIt
+  inline def states: Iterator[State] = g.nodes.iterator
   inline def proper_states: Iterator[State] = states.filter(_ != init)
-  inline def connect(src: State, dst: State, props: Set[Prop]): Unit = props.foreach(prop => g.connect(src, dst, prop))
+  inline def connect(src: State, dst: State, props: Set[Prop]): Unit = g.connect(src, dst, props)
 
   def plot(): Unit =
     println(s"${init.nodeId} [peripheries=2];")
     for n <- proper_states do
       println(s"${n.nodeId};")
-    for (s, t) <- g.edgeIt do
-      println(s"${s.nodeId} -> ${t.nodeId} [label=\"${g.labeling(s, t).mkString(" ")}\"];")
+    for (s, ts) <- g.outgoing; t <- ts; l <- g.labeling(s, t) do
+      println(s"${s.nodeId} -> ${t.nodeId} [label=\"$l\"];")
 
 
-  def rewriteSameOutgoing(): Iterator[DNIELMG[Prop]] =
+  def rewriteSameOutgoing(): Iterator[DNIELMG[Set[Prop]]] =
     val lhs = DNIELMG[Char]()
     val l = lhs.newNodes(2)
 
@@ -37,7 +37,7 @@ class BA[Prop]:
     RewriteRule(lpg, rpg, Set(0 -> 0, 1 -> 1, 2 -> 2, 3 -> 2, 4 -> 3, 5 -> 3)).applyIt(g)
 
   
-  def rewriteSameIncoming(): Iterator[DNIELMG[Prop]] =
+  def rewriteSameIncoming(): Iterator[DNIELMG[Set[Prop]]] =
     val lhs = DNIELMG[Char]()
     val l = lhs.newNodes(2)
 
@@ -51,7 +51,7 @@ class BA[Prop]:
     RewriteRule(lpg, rpg, Set(0 -> 0, 1 -> 0, 2 -> 1, 3 -> 1, 4 -> 2, 5 -> 3)).applyIt(g)
 
 
-  def rewriteIntoSelf(): Iterator[DNIELMG[Prop]] =
+  def rewriteIntoSelf(): Iterator[DNIELMG[Set[Prop]]] =
     val lhs = DNIELMG[Char]()
     val l = lhs.newNodes(2)
 
@@ -67,7 +67,7 @@ class BA[Prop]:
     RewriteRule(lpg, rpg, Set(0 -> 0, 1 -> 0, 2 -> 1, 3 -> 2, 4 -> 3, 5 -> 3)).applyIt(g)
 
 
-  def rewriteOutSelf(): Iterator[DNIELMG[Prop]] =
+  def rewriteOutSelf(): Iterator[DNIELMG[Set[Prop]]] =
     val lhs = DNIELMG[Char]()
     val l = lhs.newNodes(2)
 
@@ -81,3 +81,4 @@ class BA[Prop]:
     val rpg = PatchG[Char, rhs.type](rhs)(List(r(0) -> r(0), C -> r(0), r(0) -> C, r(0) -> C))
 
     RewriteRule(lpg, rpg, Set(0 -> 0, 1 -> 0, 2 -> 1, 3 -> 1, 4 -> 2, 5 -> 3)).applyIt(g)
+
